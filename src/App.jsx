749c819ed1d487df1411109e8c42d2f2fbd5af82
Malvikagaur <malvikagaurmg@gmail.com>
2024-09-09 17:49:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Head from "./components/Head"
+
+
+function App() {
+  
+
+  return (
+      <div>
+      <Head/>
+      </div>
+     
+    
+  )
+}
+
+export default App
---
+++ src/components/Head.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <div className='w-full shadow-lg h-10'>
+        Head
+    </div>
+  )
+}
+
+export default Head
